08f3972f8248f71b5b5809195b484e7fced3de09
0ffman <Snow88ilike@gmail.com>
2018-07-28 13:49:00 +0300

    HEADER + BANNER
    Добавлен скрипт для меню в хедере + медиа для секции banner

--- js/main.js
+++ js/main.js
@@ -4,4 +4,19 @@ var swiper = new Swiper('.swiper-container', {
         clickable: true,
         bulletElement: 'button'
       },
+      breakpoints: {// настройки для разных разрешений
+            767: {
+                slidesPerView: '1'
+              },
+        }
     });
+
+$(function() {
+        $('.btn-toggle').on('click', function() {
+        $('.main-list').slideToggle(300, function() {
+      if ($(this).css('display') === 'none'){
+        $(this).removeAttr('style');
+      }
+      });
+    });
+  });
